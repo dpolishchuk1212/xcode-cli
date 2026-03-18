576ec492775eab2125d158ab8554e6c6ecef7613
dmytropolishchuk <d.polishchuk@letyshops.com>
2026-03-18 16:35:47 +0200

    Bump to v0.1.2

--- Sources/xcode-cli/XcodeCLI.swift
+++ Sources/xcode-cli/XcodeCLI.swift
@@ -5,7 +5,7 @@ struct XcodeCLI: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "xcode-cli",
         abstract: "Token-efficient Xcode CLI for coding agents",
-        version: "0.1.1",
+        version: "0.1.2",
         subcommands: [BuildCommand.self, RunCommand.self]
     )
 }
